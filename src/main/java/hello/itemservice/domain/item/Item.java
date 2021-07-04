ce75e5fdda84173970d410b0c16353afdc064abf
ressKim <sh21dlat@gmail.com>
2021-07-04 20:10:06 +0900

    validation build.gradle add and Bean Validation study start

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    /**
+     * Range 는 hibernate.validator 인데 보통
+     * 이거를 사용하니 사용해도 무방할 것 같다.
+     */
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
