ffc98726723d0fcf148c0ee314f8c43c4e47282d
ressKim <sh21dlat@gmail.com>
2021-07-07 21:29:53 +0900

    bean validation - http message convertor 적용해 보기 study

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,34 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    /**
+     * 지금은 에러 있을 시 그냥 다 에러라고 반환하는데
+     * 나중에 exception 처리 해서 필요한 것들만 반환해야된다.
+     */
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) {
+        log.info("API 컨트롤러 호출");
+
+        if (bindingResult.hasErrors()) {
+            log.info("검증 오류 발생 errors={}", bindingResult);
+            return bindingResult.getAllErrors();
+        }
+
+        log.info("success logic");
+        return form;
+    }
+
+}
